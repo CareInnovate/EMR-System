b8565e61613f3fd4cf32e35a9ce92b64853f804c
Patrix <a0911991679@gmail.com>
2024-08-07 17:47:26 +0300

    Made the hamburger menu functional

--- app/_components/Aside.tsx
+++ app/_components/Aside.tsx
@@ -1,10 +1,8 @@
 "use client";
 import {
 	faBars,
-	faBurger,
 	faClock,
 	faDollar,
-	faHamburger,
 	faHome,
 	faPerson,
 } from "@fortawesome/free-solid-svg-icons";
@@ -12,6 +10,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { poppins } from "../fonts";
+import { useState } from "react";
 
 const Aside = () => {
 	const path = usePathname();
@@ -21,15 +20,27 @@ const Aside = () => {
 		{ path: "/profile", title: "Profile", icon: faPerson },
 		{ path: "/payment", title: "Payment", icon: faDollar },
 	];
+	const [open, setOpen] = useState(false);
 	return (
-		<aside className="absolute top-0 w-72 p-5 flex flex-col gap-2 text-lg left-0 bg-white h-full ">
-			<div className="mb-8 p-2 flex justify-between items-center">
-				<Link href={"/"} className=" text-5xl font-bold">
-					EMR
-				</Link>
+		<aside
+			className={`absolute top-0 ${
+				open ? "w-72" : "w-24"
+			} p-5 flex flex-col gap-2 text-lg left-0 bg-white h-full transition-all`}
+		>
+			<div
+				className={`mb-8 p-2 flex ${
+					open ? "justify-between" : "justify-center"
+				} items-center`}
+			>
+				{open && (
+					<Link href={"/"} className={"text-5xl font-bold"}>
+						EMR
+					</Link>
+				)}
 				<FontAwesomeIcon
 					icon={faBars}
 					className="text-2xl cursor-pointer"
+					onClick={() => setOpen((prev) => !prev)}
 				/>
 			</div>
 			{tabs.map((val, ind) => {
@@ -37,16 +48,16 @@ const Aside = () => {
 					<Link
 						key={ind}
 						href={val.path}
-						className={`px-5 py-4 cursor-pointer rounded-lg flex gap-5 items-center  ${
+						className={` ${
 							poppins.className
-						} ${
+						} px-5 py-4 cursor-pointer rounded-lg flex gap-5 items-center   ${
 							val.path === path
 								? "bg-blue-950 text-white hover:bg-blue-900"
 								: "hover:bg-blue-100"
-						}`}
+						} ${open ? "" : "justify-center text-2xl"}`}
 					>
 						<FontAwesomeIcon icon={val.icon} />
-						{val.title}
+						{open && val.title}
 					</Link>
 				);
 			})}
