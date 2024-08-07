f4121ef21fb93cbd936f74a904afcad49aa4c910
Patrix <a0911991679@gmail.com>
2024-08-07 16:27:12 +0300

    Created a simple profile menu

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -1,10 +1,12 @@
 import Tabs from "./Tabs";
+import ProfileMenu from "./ProfileMenu";
 
 const Nav = () => {
 	return (
 		<nav className="h-24 shadow-lg flex flex-col items-center justify-center px-5 mb-10 relative">
 			<div className="w-full flex justify-between items-center text-5xl font-bold">
-				EMR
+				<div>EMR</div>
+				<ProfileMenu />
 			</div>
 			<Tabs />
 		</nav>
---
+++ app/_components/ProfileMenu.tsx
@@ -0,0 +1,41 @@
+"use client";
+import {
+	faLock,
+	faMoon,
+	faUser,
+	faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+const ProfileMenu = () => {
+	const [open, setOpen] = useState(false);
+	return (
+		<div className="relative flex flex-col">
+			<FontAwesomeIcon
+				icon={faUserCircle}
+				width={30}
+				className="cursor-pointer"
+				onClick={() => setOpen((prev) => !prev)}
+			/>
+			{open && (
+				<div className="absolute top-16 right-0 flex flex-col w-max text-lg font-sans justify-start items-start bg-slate-100 rounded-md shadow">
+					<div className="w-full hover:bg-blue-950 hover:text-white px-5 py-2  cursor-pointer rounded-md flex gap-3 items-center">
+						<FontAwesomeIcon icon={faMoon} />
+						<span>Dark Mode</span>
+					</div>
+					<div className="w-full hover:bg-blue-950 hover:text-white px-5 py-2 cursor-pointer rounded-md flex gap-3 items-center">
+						<FontAwesomeIcon icon={faLock} />
+						<span>Change Password</span>
+					</div>
+					<div className="w-full hover:bg-blue-950 hover:text-white px-5 py-2 cursor-pointer rounded-md flex gap-3 items-center">
+						<FontAwesomeIcon icon={faUser} />
+						<span>Logout</span>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default ProfileMenu;
